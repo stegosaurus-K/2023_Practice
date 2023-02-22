c7e829a7d10239722a4d40b0262704011ccdaafe
jianlong.wei <331851471@qq.com>
2023-02-22 23:01:15 +0800

    146diff

--- LeetCode_Hot_100/146.js
+++ LeetCode_Hot_100/146.js
@@ -1,4 +1,3 @@
-// 定义一个创建节点的方法
 class ListNode {
   constructor(key, value) {
     this.key = key;
@@ -7,11 +6,10 @@ class ListNode {
     this.prev = null;
   }
 }
-// 实现 LRUCache 类
 class LRUCache {
   constructor(capacity) {
     this.capacity = capacity;
-    this.hashTable = {};
+    this.hash = {};
     this.count = 0;
     this.dummyHead = new ListNode();
     this.dummyTail = new ListNode();
@@ -19,12 +17,12 @@ class LRUCache {
     this.dummyTail.prev = this.dummyHead;
   }
   get(key) {
-    let node = this.hashTable[key];
+    let node = this.hash[key];
     if (node == null) return -1;
-    this.moveTohead(node);
+    this.moveToHead(node);
     return node.value;
   }
-  moveTohead(node) {
+  moveToHead(node) {
     this.removeFromList(node);
     this.addToHead(node);
   }
@@ -38,27 +36,25 @@ class LRUCache {
     node.prev = this.dummyHead;
     node.next = this.dummyHead.next;
     this.dummyHead.next.prev = node;
-    this.dummyHead.next = node;
+    this.dummy.next = node;
   }
   put(key, value) {
-    let node = this.hashTable[key];
+    let node = this.hash[key];
     if (node == null) {
       let newNode = new ListNode(key, value);
-      this.hashTable[key] = newNode;
+
+      this.hash[key] = newNode;
+      this.moveToHead(newNode);
       this.count++;
-      this.addToHead(newNode);
-      if (this.count > this.capacity) {
-        this.removeLRUItem();
-      }
+      if (this.count > this.capacity) removeLRUItem();
     } else {
       node.value = value;
-      this.moveTohead(node);
+      this.moveToHead(node);
     }
   }
-
   removeLRUItem() {
     let tail = this.popTail();
-    delete this.hashTable[tail.key]; // 哈希表存储的是key
+    delete this.hash[tail.key];
     this.count--;
   }
   popTail() {
